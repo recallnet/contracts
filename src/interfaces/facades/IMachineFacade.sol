f388b532280d8502dca84546959e183b53300170
Sander Pick <sanderpick@gmail.com>
2025-02-18 17:15:33 -0800

    fix: include machine kind in machine events
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- src/interfaces/facades/IMachineFacade.sol
+++ src/interfaces/facades/IMachineFacade.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.26;
 
 interface IMachineFacade {
     /// @dev Emitted when a machine is created.
+    /// @param kind Machine kind, i.e., bucket (0) or timehub (1).
     /// @param owner Machine owner address.
     /// @param metadata IPLD-encoded machine metadata (HashMap<String, String>).
-    event MachineCreated(address indexed owner, bytes metadata);
+    event MachineCreated(uint8 indexed kind, address indexed owner, bytes metadata);
 
     /// @dev Emitted when a machine is initialized.
+    /// @param kind Machine kind, i.e., bucket (0) or timehub (1).
     /// @param machineAddress Machine address.
-    event MachineInitialized(address machineAddress);
+    event MachineInitialized(uint8 indexed kind, address machineAddress);
 }
