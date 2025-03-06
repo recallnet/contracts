e549155f43405838640bb053a67da4c9297dcc2d
Sergey Ukustov <sergey@ukstv.me>
2025-03-06 20:11:19 +0300

    ObjectMetadataUpdated

--- crates/facade/src/lib.rs
+++ crates/facade/src/lib.rs
@@ -140,19 +140,6 @@ pub mod bucket {
     use anyhow::Result;
     use std::collections::HashMap;
 
-    pub fn object_metadata_updated(
-        key: Vec<u8>,
-        metadata: &HashMap<String, String>,
-    ) -> Result<IBucketFacadeEvents> {
-        let metadata = fvm_ipld_encoding::to_vec(metadata)?;
-        Ok(IBucketFacadeEvents::ObjectMetadataUpdated(
-            ObjectMetadataUpdated {
-                key: key.into(),
-                metadata: metadata.into(),
-            },
-        ))
-    }
-
     pub fn object_deleted(key: Vec<u8>, blob_hash: &[u8; 32]) -> Result<IBucketFacadeEvents> {
         Ok(IBucketFacadeEvents::ObjectDeleted(ObjectDeleted {
             key: key.into(),
