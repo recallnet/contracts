3a375af41355dae9f8109513d099a932ade1b94c
Sergey Ukustov <sergey@ukstv.me>
2025-03-06 20:08:05 +0300

    ObjectAdded

--- crates/facade/src/lib.rs
+++ crates/facade/src/lib.rs
@@ -129,24 +129,16 @@ pub mod blobs {
 mod bucket_facade;
 #[cfg(feature = "bucket")]
 pub mod bucket {
+    pub type Event = crate::bucket_facade::ibucketfacade::IBucketFacade::IBucketFacadeEvents;
+    pub type ObjectAdded = crate::bucket_facade::ibucketfacade::IBucketFacade::ObjectAdded;
+    pub type ObjectDeleted = crate::bucket_facade::ibucketfacade::IBucketFacade::ObjectDeleted;
+    pub type ObjectMetadataUpdated = crate::bucket_facade::ibucketfacade::IBucketFacade::ObjectMetadataUpdated;
+
     use crate::bucket_facade::ibucketfacade::IBucketFacade::{
-        IBucketFacadeEvents, ObjectAdded, ObjectDeleted, ObjectMetadataUpdated,
+        IBucketFacadeEvents,
     };
     use anyhow::Result;
     use std::collections::HashMap;
-
-    pub fn object_added(
-        key: Vec<u8>,
-        blob_hash: &[u8; 32],
-        metadata: &HashMap<String, String>,
-    ) -> Result<IBucketFacadeEvents> {
-        let metadata = fvm_ipld_encoding::to_vec(metadata)?;
-        Ok(IBucketFacadeEvents::ObjectAdded(ObjectAdded {
-            key: key.into(),
-            blobHash: blob_hash.into(),
-            metadata: metadata.into(),
-        }))
-    }
 
     pub fn object_metadata_updated(
         key: Vec<u8>,
