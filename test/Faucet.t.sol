cd67dd81a4eae2a315d3d72924ad0b2ec35c9244
Aaron Sutula <asutula@users.noreply.github.com>
2025-04-01 11:00:22 -0600

    format fix

--- test/Faucet.t.sol
+++ test/Faucet.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.26;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Test, Vm} from "forge-std/Test.sol";
 
 import {DeployScript as FaucetDeployer} from "../script/Faucet.s.sol";
